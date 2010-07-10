06faff141d6feddadc06bceaaa593af7bc46bdcd
mpvanwinkle77 <mpvanwinkle77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-10 15:27:50 +0000

    Bug fixes: Proper registration of style sheet, Options handled by register settings API, added new parameters (message and notify) to shortcode handler
    git-svn-id: https://plugins.svn.wordpress.org/simplr-registration-form/trunk@262332 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simplr_reg_page.php
+++ simplr_reg_page.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Simplr User Registration Form	
-Version: 0.1.1
+Version: 0.1.2
 Description: This a simple plugin for adding a custom user registration form to any post or page using shortcode.
 Author: Mike Van Winkle
 Author URI: http://www.mikevanwinkle.com
@@ -17,131 +17,151 @@ if(version_compare($wp_version, "2.8", "<")) { exit($exit_msg); }
 
 define("SIMPLR_DIR", WP_PLUGIN_URL . '/simplr-registration-form/' );
 
-/*create admin page*/
-function simplr_reg_admin() {
-//Add posted options
-if(isset($_POST['submit'])) {
-	if(check_admin_referer('-1','sreg-nonce-test')) {
-		//Add Options
-		$options = array(
-			"sreg_admin_email" => $_POST['sreg_admin_email'],
-			"sreg_email" => $_POST['sreg_email'],
-			"sreg_style" => $_POST['sreg_style']
-			);
-			foreach($options as $key => $value) {
-					update_option($key,$value); 
-			}//end foreach
-		}//Close nonce check 
-	}//Close POST conditional
+function simplr_reg_set() { ?>
+	<div class="wrap">
+		  <h2>Registration Form Settings</h2>
+			  <form method="post" action="options.php" id="simplr-settings">
+				  <table class="form-table">
+				  <tr valign="top">
+				  <th scope="row">Set Default FROM Email:</th>
+				  <td><input type="text" name="sreg_admin_email" value="<?php echo get_option('sreg_admin_email'); ?>" class="field text wide"/></td>
+				  </tr>
+				  <tr valign="top">
+				  <th scope="row">Set Defult Confirmation Message:<br>
+				  <small></small>
+				  </th>
+				  <td>
+				  <textarea id="sreg_email" name="sreg_email" style="width:500px;height:200px; padding:3px;" class="sreg_email"><?php echo get_option('sreg_email'); ?></textarea></td>
+				  </tr>
+				  <tr valign="top">
+				  <th scope="row">Stylesheet Override</th>
+				  <td>
+				  <input type="text" name="sreg_style" value="<?php echo get_option('sreg_style'); ?>" class="field text wide" />
+				  <p><small>Enter the URL of the stylesheet you would prefer to use. Leave blank to stick with default.</small></p>
+				  </td>
+				  </tr>
+				  </table>
+			  <?php settings_fields('simplr_reg_options'); ?>
+			  <p class="submit">
+			  <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			  </p>
+			  </form>
+		<div id="instructions">
+			<h2>How to use</h2>
+			<p>The goal of this plugin is to give developers and advanced Wordpress users a simple way to create role-specific registration forms for their wordpress website. For instance, you might be running an education based site in which you wanted both teachers and students to particape. This plugin enables you to create distinct registration forms for each type of registrant.</p>
+			<p>Because the focus is on seperating registrants, I have not focused on creating a highly customizable form like <a href="http://wordpress.org/extend/plugins/register-plus/" title="Register Plus">Register Plus</a>. </p>
+			<h3>Usage</h3>
+			<p>To use this plugin simplr employ the shortcode <code>[Register]</code> on any Wordpress post or page. The default role is "subscriber". To apply another role to the registration simply use the the role parameter, for instance: <code>[Regsiter role="editor"]</code>. If you have created custom roles you may use them as well. </p>
+			<p>You can also use shortcode so specify a custom confirmation message for each form: <br>
+			<code>[Register role="teacher" <b>message="Thank you for registering for my site. If you would like to encourage your students to register, please direct them to http://www.domain.com/students"</b>]</code></p>
+			<p>Finally, you can specify emails to be notified when a new user is registered. By defualt site admins will receive notice but to notify other simply use the notify parameter:
+			<code>[Register role="teacher" message="Thank you for registering for my site. If you would like to encourage your students to register, please direct them to http://www.domain.com/students" <b>notify="email1@email.com,email2@email.com"</b>]</code>
+		<p>
+		</div>
+	  </div>
+  <?php
+}//End Function
 
-//setup options form
-global $options;
-?>
-<div class="wrap">
-    <link href="simplr_reg_admin.css" rel="stylesheet" type="css/text">
-	<?php screen_icon(); ?>
-	<h2>Registration Form Settings</h2>
-	<p>Use this page to configure your registration page settings. To deploy registration page simply use the shortcode [Register] in any page. You can also override the role of the registrant by using the parameter "role=". For instance [Register role="contributor"].</p>
-	<form action="" method="post" id="simplr_reg_settings">
-	<h3><label for="sreg_admin_email">FROM address for notification email</label></h3>
-	<input type="text" name="sreg_admin_email" id="sreg_admin_email" value="<?php echo esc_attr(get_option('sreg_admin_email')); ?>" style="width:300px;"></input>
-	<h3><label for="sreg_email">Confirmation Message</label></h3>
-	<p><textarea name="sreg_email" id="sreg_email" style="width:300px;height:200px;"><?php echo esc_attr(get_option('sreg_email')); ?></textarea></p>
-	<h3><label for="sreg_style">Stylesheet Override</label></h3>
-	<small>Enter the URL of the stylesheet you would prefer to use. Leave blank to stick with default.</small>
-	<p><input type="text" name="sreg_style" id="sreg_style" value="<?php echo esc_attr(get_option('sreg_style')); ?>" style="width:300px;"></input></p>
-	<p class="submit"><input type="submit" name="submit" value="Update Options &raquo;"></p>
-	<?php wp_nonce_field('-1','sreg-nonce-test'); ?>
-	</form>
-	</div>
-<?php 
-
-// End admin page function
+function simplr_reg_menu() {
+	$page = add_submenu_page('options-general.php','Registration Forms', 'Registration Forms','manage_options','simplr_reg_set', 'simplr_reg_set');
+	add_action('admin_print_styles-' . $page, 'simplr_admin_style');
+	register_setting ('simplr_reg_options', 'sreg_admin_email', '');
+	register_setting ('simplr_reg_options', 'sreg_email', '');
+	register_setting ('simplr_reg_options', 'sreg_style', '');
 }
 
-function sreg_styles() {
-global $options;
-$src = SIMPLR_DIR .'/simplr_reg.css';
-$style = '<link rel="stylesheet" type="text/css" media="screen,projection" href="' .$src .'" />';
-return $style;
+function simplr_reg_styles() {
+	global $options;
+	$style = get_option('sreg_style');
+	if(!$style) {
+		$src = SIMPLR_DIR .'simplr_reg.css';
+		wp_register_style('simplr-forms-style',$src);
+		wp_enqueue_style('simplr-forms-style');
+	} else {
+		$src = $style;
+		wp_register_style('simplr-forms-custom-style',$src);
+		wp_enqueue_style('simplr-forms-custom-style');
+	}
 //End Function
 }
 
+function simplr_admin_style() {
+	$src = SIMPLR_DIR . 'simplr_admin.css';
+	wp_register_style('simplr-admin-style',$src); 
+	wp_enqueue_style('simplr-admin-style');
+}
 
 //Register Menu Item for Admin Page
 function simplr_reg_admin_page() {
 	add_submenu_page('options-general.php','Registration Page Settings', 'Registration Page','manage_options','simplr_reg_page', 'simplr_reg_admin');
 	}	
 
-function sreg_process_form($role) {
-	global $options;
-	$admin_email = get_option('sreg_admin_email');
-	$emessage = get_option('sreg_email');
-	require_once(ABSPATH . WPINC . '/registration.php' );
-	require_once(ABSPATH . WPINC . '/pluggable.php' );
-	//Assign POST variables
-	$user_name = $_POST['username'];
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$user_name = sanitize_user($user_name, true);
-	$email = $_POST['email'];
-	//Test variables using built in wordpress functions.
-	$user_test = validate_username($user_name);
-		if($user_test != true) {
-			$message = 'Invalid Username';
+function sreg_process_form($atts) {
+		global $options;
+		$admin_email = $atts['from'];
+		$emessage = $atts['message'];
+		require_once(ABSPATH . WPINC . '/registration.php' );
+		require_once(ABSPATH . WPINC . '/pluggable.php' );
+		//Assign POST variables
+		$user_name = $_POST['username'];
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$user_name = sanitize_user($user_name, true);
+		$email = $_POST['email'];
+		//Test variables using built in wordpress functions.
+		$user_test = validate_username($user_name);
+			if($user_test != true) {
+				$message = 'Invalid Username';
+				}
+			$email_test = email_exists($email);
+			if($email_test != false) {
+					$message = 'An account with this email has already been registered';
+				}
+			$user_id = username_exists( $user_name );
+			if($user_id) {
+				$message = 'This username already exists';
 			}
-		$email_test = email_exists($email);
-		if($email_test != false) {
-				$message = 'An account with this email has already been registered';
-			}
-		$user_id = username_exists( $user_name );
-		if($user_id) {
-			$message = 'This username already exists';
-		}
-		if (!$message) {
-		//This part actually generates the account
-			$random_password = wp_generate_password( 12, false );
-			$userdata = array(
-				'user_login' => $user_name,
-				'first_name' => $fname,
-				'last_name' => $lname,
-				'user_pass' => $random_password,
-				'user_email' => $email,
-				'role' => $role
-				);
-			$user_id = wp_insert_user( $userdata );
-			$message = 'Registration Successful. A password was sent to you via email.';
-			$message = '<div class="simplr-message">'.$message .'</div>';
-			//add flag for the user to change their auto-generated password
-			$update = update_user_option($user_id, 'default_password_nag', true, true);
-			//notify admin of new user
-			$notify = wp_new_user_notification($user_id, $random_pass);
-			$site = get_option('siteurl');
-			$name = get_option('blogname');
-				if(empty($emessage)) {$emessage = "Thanks for signing up for $name. Here is your password. You should login and change it as soon as possible.\r\rUsername: $user_name\rPassword: $random_password\rLogin: $site";}
-					$emessage = $emessage . "\r\r---\rYou should login and change it as soon as possible.\r\rUsername: $user_name\rPassword: $random_password\rLogin: $site";
-					$mail_message = wp_mail($email, 'Registration Confirmaion', $emessage);
-					echo "$message";
-				} else { 
-				//Print the appropriate message
+			if (!$message) {
+			//This part actually generates the account
+				$random_password = wp_generate_password( 12, false );
+				$userdata = array(
+					'user_login' => $user_name,
+					'first_name' => $fname,
+					'last_name' => $lname,
+					'user_pass' => $random_password,
+					'user_email' => $email,
+					'role' => $atts['role']
+					);
+				$user_id = wp_insert_user( $userdata );
+				$message = 'Registration Successful. A password was sent to you via email.';
 				$message = '<div class="simplr-message">'.$message .'</div>';
-				echo $message;
-		}
+				//add flag for the user to change their auto-generated password
+				$update = update_user_option($user_id, 'default_password_nag', true, true);
+				//notify admin of new user
+				$notification = wp_new_user_notification($user_id, $random_pass); 
+				$site = get_option('siteurl');
+				$name = get_option('blogname');
+				$notify = $atts['notify'];
+					mail($notify, "New User Registered for $name", "A new user has registered for $name.\rUsername: $user_name\r Email: $email \r");
+				$emessage = $emessage . "\r\r---\rYou should login and change your password as soon as possible.\r\rUsername: $user_name\rPassword: $random_password\rLogin: $site";
+				$mail_message = wp_mail($email, "$name - Registration Confirmaion", $emessage);
+				echo "Your Registration was successful, please check your email for confirmation";
+			} else { 
+			//Print the appropriate message
+			$message = '<div class="simplr-message">'.$message .'</div>';
+			echo $message;
+			}
 //END FUNCTION
 }
 
-function sreg_basic($role) {
+function sreg_basic($atts) {
 	//Check if the user is logged in, if so he doesn't need the registration page
-	global $options;
-	echo sreg_styles();
-	$admin_email = get_option('admin_email');
-	$emessage = get_option('sreg_email');
-	if ( is_user_logged_in() ) {
-		echo "You are already registered for this site!!!";
-	} else {
-		//Then I check to see whether a form has been submitted, if so, I deal with it.
-		if(isset($_POST['submit'])) {
-			sreg_process_form($role);	
+		if ( is_user_logged_in() ) {
+			echo "You are already registered for this site!!!";
+		} else {
+		//Then check to see whether a form has been submitted, if so, I deal with it.
+		if(isset($_POST['submit-reg'])) {
+			sreg_process_form($atts);	
 		} else {
 			//POST FORM
 			echo '<div id="simplr-form">';
@@ -162,7 +182,7 @@ function sreg_basic($role) {
 			echo '<label for="email" class="left">Enter your email:</label>';
 			echo '<input type="text" name="email" class="right"><br/>';
 			echo '</div>';
-			echo '<input type="submit" name="submit" value="Register" class="submit">';
+			echo '<input type="submit" name="submit-reg" value="Register" class="submit">';
 			wp_nonce_field('-1','simplr-nonce' );
 			echo '</form>';
 			echo '</div>';
@@ -174,21 +194,24 @@ function sreg_basic($role) {
 
 //this function determines which version of the registration to call
 function sreg_figure($atts) {
+	global $options;
 	extract(shortcode_atts(array(
 	'role' => 'subscriber',
-	'advanced' => 'false',
-	'fb' => 'false',
+	'from' => get_option('sreg_admin_email'),
+	'message' => get_option('sreg_email'),
+	'notify' => get_option('sreg_email'),
+	'fb' => false,
 	), $atts));
-if($role != 'admin') {
-$function = sreg_basic($role);
-} else { 
-$function = 'You should not register admin users via a public form';
-}
-//End Function
-}
+		if($role != 'admin') {
+			$function = sreg_basic($atts);
+		} else { 
+			$function = 'You should not register admin users via a public form';
+		}
+}//End Function
 
 
-add_action('admin_menu','simplr_reg_admin_page');
+add_action('wp_print_styles','simplr_reg_styles');
+add_action('admin_menu','simplr_reg_menu');
 add_shortcode('Register', 'sreg_figure');
 
 ?>
