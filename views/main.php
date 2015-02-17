64f619d79a545bdb2a180f41b6bd2926f19647b0
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 15:50:01 +0000

    More strings
    git-svn-id: https://plugins.svn.wordpress.org/simplr-registration-form/trunk@1092527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/main.php
+++ views/main.php
@@ -18,13 +18,13 @@ if(isset($data['main-submit'])) {
 }
 ?>
 <form action="?page=simplr_reg_set&regview=main" method="post" id="add-field">
-	<h3>Core Settings</h3>
+	<h3><?php _e('Core Settings','simplr-reg'); ?></h3>
 	<?php
 	SREG_Form::related_select(array(
 		'name'=>'register_redirect',
-		'label'=>'Redirect default signup action to this page',
+		'label'=>__('Redirect default signup action to this page','simplr-reg'),
 		'required'=>false,
-		'post_type'=>'page','comment'=>'Use this option to redirect the default WordPress wp-login.php?action=redirect action. This will not redirect your login page as a whole.'),
+		'post_type'=>'page','comment'=>__('Use this option to redirect the default WordPress wp-login.php?action=redirect action. This will not redirect your login page as a whole.','simplr-reg')),
 		@$simplr_reg->register_redirect,
 		'wide chzn'
 	);
@@ -40,34 +40,34 @@ if(isset($data['main-submit'])) {
 
 	SREG_Form::related_select(array(
 		'name'=>'profile_redirect',
-		'label'=>'Redirect default profile page',
+		'label'=>__('Redirect default profile page','simplr-reg'),
 		'required'=>false,
 		'post_type'=>'page',
-		'comment'=>'Use this option to force users to access only their "front" end profile page ... i.e. the page with the [profile_page] shortcode'),
+		'comment'=>__('Use this option to force users to access only their "front" end profile page ... i.e. the page with the [profile_page] shortcode','simplr-reg')),
 		@$simplr_reg->profile_redirect,
 		'wide chzn'
 		);
 
 	SREG_Form::related_select(array(
 		'name'			=>'thank_you',
-		'label'			=>'Custom Thank You Page',
+		'label'			=>__('Custom Thank You Page','simplr-reg'),
 		'required'	=>false,
 		'post_type'	=>'page',
-		'comment' 	=> 'You can leave this field blank. The default behavior outputs a confirmation on the page which the registration shortcode is save. Use this only if you want to redirect the user elsewhere after registration.'),
+		'comment' 	=> __('You can leave this field blank. The default behavior outputs a confirmation on the page which the registration shortcode is save. Use this only if you want to redirect the user elsewhere after registration.','simplr-reg')),
 		@$simplr_reg->thank_you,
 		'wide chzn',''
 	);
 
 	SREG_Form::text(array(
 		'name'=>'default_email',
-		'label'=>'Default FROM Email',
+		'label'=>__('Default FROM Email','simplr-reg'),
 		'required'=>false
 	),
 	$simplr_reg->default_email?$simplr_reg->default_email:get_option('admin_email'), 'wide'
 	);
 	SREG_Form::radio(array(
 		'name'=>'styles',
-		'label'=>'Disable Plugin Styles',
+		'label'=>__('Disable Plugin Styles','simplr-reg'),
 		'required'=>'no',
 		'default' => 'no'
 		), @$simplr_reg->styles, '',
@@ -77,7 +77,7 @@ if( $simplr_reg->styles != 'yes' ):
 
 	SREG_Form::select(array(
 		'name'=>'style_skin',
-		'label'	=> 'Select a style',
+		'label'	=> __('Select a style','simplr-reg'),
 		'required'	=> 'no',
 		'default'	=> 'default.css',
 		'helper'	=> 'skins'),
@@ -85,9 +85,9 @@ if( $simplr_reg->styles != 'yes' ):
 
 	SREG_Form::text(array(
 		'name'=>'stylesheet',
-		'label'=>'Override Default Stylesheet',
+		'label'=>__('Override Default Stylesheet','simplr-reg'),
 		'required'=>false,
-		'comment'=>'Specify a custom stylesheet. Will not apply if "Plugin Styles" are disabled'
+		'comment'=>__('Specify a custom stylesheet. Will not apply if "Plugin Styles" are disabled','simplr-reg')
 	),
 	@$simplr_reg->stylesheet, 'wide'
 	);
@@ -104,6 +104,6 @@ endif;
 	?>
 	<?php echo wp_nonce_field(-1,"reg-main"); ?>
 	<p class="submit">
-		<input type="submit" class="button-primary" name="main-submit" value="<?php _e('Save Changes') ?>" />
+		<input type="submit" class="button-primary" name="main-submit" value="<?php _e('Save Changes','simplr-reg') ?>" />
 	</p>
 </form>
