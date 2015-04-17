8a2e1e1b58c3fb3d99809988292c7e86ceb09fc0
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 15:52:39 +0000

    Tag 2.3.3
    git-svn-id: https://plugins.svn.wordpress.org/simplr-registration-form/tags/2.3.3@1137316 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simplr_reg.js
+++ simplr_reg.js
@@ -2,14 +2,14 @@
 (function(){
 	// creates the plugin
 	root = simplr.plugin_dir;
-	
+
 	tinymce.create('tinymce.plugins.simplr_reg', {
 		// creates control instances based on the control's id.
 		// our button's id is &quot;reg_button&quot;
 		createControl : function(id, controlManager) {
 			if (id == 'simplr_reg_button') {
 				// creates the button
-				var button = controlManager.createButton('simpr_reg_button', {
+				var button = controlManager.createButton('simplr_reg_button', {
 					title : 'Registration Form', // title of the button
 					image : root+'registration_24x24.png',  // path to the button's image
 					onclick : function() {
@@ -25,20 +25,18 @@
 			return null;
 		}
 	});
-	
-	
+
+
 	// registers the plugin. DON'T MISS THIS STEP!!!
 	tinymce.PluginManager.add('simplr_reg', tinymce.plugins.simplr_reg);
-	
+
 	// executes this when the DOM is ready
 	jQuery(function(){
 		// creates a form to be displayed everytime the button is clicked
 		jQuery('#footer').append(function() {
-				jQuery(this).after('<div id="hook"></div>');
-				jQuery('#hook').load(simplr.plugin_dir + 'simplr_reg_options.php');
-			});
-	
+			jQuery(this).after('<div id="hook"></div>');
+			jQuery('#hook').load(simplr.plugin_dir + 'simplr_reg_options.php');
+		});
 	});
 
-	
 })()
